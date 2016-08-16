f124f8d7df829e703452585dab3c9d521b08c337
Oleg <OlejekG@ukr.net>
2016-08-16 16:09:19 +0300

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,7 @@
 
 $(document).ready(function(){
-$('#Shapka').hide(1500); //Автоматический цикл.
-$('#Shapka').show(1500);
+  $('#Shapka').fadeOut(2500);
+  $('#Shapka').fadeIn(2500);
+//$('#Shapka').hide(1500); //Автоматический цикл.
+//$('#Shapka').show(1500);
 }); //Конец ready
